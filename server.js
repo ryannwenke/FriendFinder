7032d3445328209d230f09e21d5e85d8d7f2110d
Ryann Wenke <ryannwenke@gmail.com>
2018-02-25 22:01:02 -0700

    server

--- server.js
+++ server.js
@@ -1,3 +1,19 @@
-var port = process.env.PORT || 3000;
-...
-app.listen(port, function() {
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require("path");
+
+var app = express();
+
+var PORT = process.env.PORT || 8080;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({ type: 'application/*+json' }))
+app.use(bodyParser.raw({ type: 'application/vnd.custom-type' }))
+app.use(bodyParser.text({ type: 'text/html' }))
+
+require("./app/routing/api-routes.js")(app);
+require("./app/routing/html-routes.js")(app);
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
